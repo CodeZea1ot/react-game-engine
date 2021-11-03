ac2f82907903efa8d8d0d4da25f4e725686a9bfc
Code_Zealot <business@codezealot.com>
2021-11-02 22:51:40 -0400

    start testing assets dir and building canvas component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import Canvas from "./components/Canvas/Canvas";
+
 function App() {
-  return <div className="App">app</div>;
+  const draw = (ctx, frameCount) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+    ctx.fill();
+  };
+
+  return (
+    <div className="App">
+      <Canvas draw={draw} />
+    </div>
+  );
 }
 
 export default App;
